93deac1fb9a2e183237541541697170294939263
Shingie Bishi <shingiebishi@gmail.com>
2020-05-11 22:32:34 +0200

    location.data.name fix

--- clientweb/src/navigation/main.js
+++ clientweb/src/navigation/main.js
@@ -1,12 +1,10 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-
+import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { HomeScreen } from "../views/home/HomeScreen";
-import { AboutScreen } from "../views/about/AboutScreen";
-import { AdminScreen } from "../views/admin/AdminScreen";
-import { StudentScreen } from "../views/student/StudentScreen";
-import { TeacherScreen } from "../views/teacher/TeacherScreen";
+import {HomeScreen} from '../views/home/HomeScreen';
+import {AboutScreen} from '../views/about/AboutScreen';
+import {AdminScreen} from '../views/admin/AdminScreen';
+import {StudentScreen} from '../views/student/StudentScreen';
+import {TeacherScreen} from '../views/teacher/TeacherScreen';
 import { AddCourseScreen } from "../views/admin/AddCourseScreen";
 import { AddTeacherScreen } from "../views/admin/AddTeacherScreen";
 import { SubscriptionScreen } from "../views/admin/SubscriptionScreen";
@@ -18,49 +16,28 @@ import { ClassroomScreen } from "../components/ClassroomScreen";
 import ClassroomStudentsScreen from "../components/ClassroomStudentsScreen";
 import { SubjectContent } from "../views/student/SubjectContent";
 
-export class Main extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={HomeScreen} />
-        <Route exact path="/home" component={HomeScreen} />
-        <Route exact path="/about" component={AboutScreen} />
-        <Route exact path="/admin" component={AdminScreen} />
-        <Route exact path="/add-course" component={AddCourseScreen} />
-        <Route exact path="/courses" component={CourseListScreen} />
-        <Route exact path="/course-outline" component={CourseOutlineScreen} />
-        <Route exact path="/subscriptions" component={SubscriptionScreen} />
 
-        <Route exact path="/add-teacher" component={AddTeacherScreen} />
-        <Route exact path="/teacher" component={TeacherScreen} />
-        <Route
-          exact
-          path="/teacher-classwork"
-          component={TeacherClassworkScreen}
-        />
-        <Route
-          exact
-          path="/teacher-students"
-          component={TeacherStudentScreen}
-        />
-        <Route exact path="/classroom" component={ClassroomScreen} />
-        <Route
-          exact
-          path="/classroom-students"
-          component={ClassroomStudentsScreen}
-        />
+const Main = () => (
+    <Switch>
+       <Route exact path="/" component={HomeScreen} />
+       <Route exact path="/home" component={HomeScreen} />
+       <Route exact path="/about" component={AboutScreen} />
+       <Route exact path="/admin" component={AdminScreen} />
+       <Route exact path="/add-course" component={AddCourseScreen} />
+       <Route exact path="/courses" component={CourseListScreen} />
+       <Route exact path="/course-outline" component={CourseOutlineScreen} />
+       <Route exact path="/subscriptions" component={SubscriptionScreen} />
 
-        <Route exact path="/student" component={StudentScreen} />
-        <Route exact path="/subject-content" component={SubjectContent} />
-      </Switch>
-    );
-  }
-}
+       <Route exact path="/add-teacher" component={AddTeacherScreen} />
+       <Route exact path="/teacher" component={TeacherScreen} />
+       <Route exact path="/teacher-classwork" component={TeacherClassworkScreen} />
+       <Route exact path="/teacher-students" component={TeacherStudentScreen} />
+       <Route exact path="/classroom" component={ClassroomScreen} />
+       <Route exact path="/classroom-students" component={ClassroomStudentsScreen} />
 
-const mapStateToProps = (state) => ({
-    ...state
-});
+       <Route exact path="/student" component={StudentScreen} />
+       <Route exact path="/subject-content" component={SubjectContent} />
+    </Switch>
+);
 
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default Main;
