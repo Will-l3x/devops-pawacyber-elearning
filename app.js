e1a2733c3259b7def72b495b84a8e94092183a2e
Tate <tatembuva@hotmail.com>
2020-05-16 21:32:07 +0200

    ...some fixes after merge

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var debug = require("debug");
 var express = require("express");
 var path = require("path");
 var favicon = require("serve-favicon");
-var logger = require("morgan");
+// var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var sql = require("mssql");
@@ -68,7 +68,7 @@ app.use(cors());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
