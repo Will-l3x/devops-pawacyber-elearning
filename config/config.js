47d07c4ab34ee3c28ab99876f9012b293276a043
Tate <tatembuva@hotmail.com>
2020-05-26 11:58:55 +0200

    ...schooladmin, and student routes on swagger

--- config/config.js
+++ config/config.js
@@ -1,28 +1,28 @@
 "use strict";
 
-//var config = {
-//    user: 'sa',
-//    password: '12345678',
-//    server: 'LOCALHOST',
-//    database: 'newschool',
-//    stream: true,
-//     options: {
-//         encrypt: true,
-//         stream: true
-//    }
-//};
-
 var config = {
-  user: "cyberadmin",
-    password: "Mcgry567@w1",
-    server: "cyberschool1.database.windows.net",
-  database: "cyberschool",
+  user: "sa",
+  password: "tate@1234",
+  server: "localhost",
+  database: "newschool4",
   stream: true,
-  options: {
-    encrypt: true,
-      stream: true,
-      enableArithAbort:true
-  }
+  // options: {
+  //     encrypt: true,
+  //     stream: true
+  // }
 };
+
+//var config = {
+//   user: "cyberadmin",
+//     password: "Mcgry567@w1",
+//     server: "cyberschool1.database.windows.net",
+//   database: "cyberschool",
+//   stream: true,
+//   options: {
+//     encrypt: true,
+//       stream: true,
+//       enableArithAbort:true
+//   }
+// };
 
 module.exports = config;
--- controllers/_schooladmin.js
+++ controllers/_schooladmin.js
@@ -88,207 +88,48 @@ let teachers = (req, res) => {
 }
 
 let add_teacher = (req, res) => {
-    var schoolid = 0;
-    var schoolname = req.body.schoolname;
-    var contacts = req.body.contacts;
-    var address = req.body.address;
-    var email = req.body.email;
-    var datejoined , activefrom = moment().format('YYYY-MM-DD');
-    var enrolmentkey = generator.generate({
-        length: 5,
-        numbers: true
-    });
+    let schoolid = req.body.schoolid;
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+    let userid = req.body.userid;
+    let datejoined = moment().format('YYYY-MM-DD');
+   
 
-    var password = generator.generate({
-        length: 8,
-        numbers: true
-    });
+    var query = `insert into [teachers] \
+    (schoolid, firstname, lastname, userid,datejoined) \
+    values(${schoolid}, ${firstname}, ${lastname}, ${userid}, ${datejoined}); \
+    select * from teachers where teachers.teacherId = SCOPE_IDENTITY(); `;
+   
+       request
+        .query(query, function (err, recordset) {
+        	let teacher = recordset.recordset;
+            if (err) {
+                console.log(err);
+                console.log(err.stack);
+                return res.json({
+                    status: 500,
+                    success: false,
+                    message: "An error occured",
+                    error: err.message
+                });
+            } else {
 
-    password = bcrypt.hashSync(password, process.env.bcrypt_salt);
-
-    var roleid = 4; //schooladmin
-    var firstname = req.body.firstname;
-    var lastname = req.body.lastname;
-    var otpexpiry = moment().add(31, 'day').format();
-    var pin = gen();
-
-    var query = "INSERT INTO [schools] \
-    (schoolname,contacts,address,email,datejoined,enrolmentkey) \
-    VALUES(@name,@contacts,@address,@email,@dj,@enrolmentkey)";
-    query = query + ';select @@IDENTITY AS \'identity\'';
-
-    var query_user = "INSERT INTO [users] \
-    (email,password,otp,roleid,otpexpiry,activefrom) \
-    VALUES(@email,@password,@otp,@roleid,@otpe,@dj)";
-    query_user = query_user + ';select @@IDENTITY AS \'identity\'';
-
-    var query_schooladmin = "INSERT INTO [schooladmins] \
-    (firstname,lastname,activefrom,userid,schoolid) \
-    VALUES(@firstname,@lastname,@dj,@userid,@schoolid)";
-
-    let query_email = "SELECT * FROM [schools] WHERE email = @email";
-
-    var transaction = new sql.Transaction();
-    transaction.begin(function (err) {
-        transaction.rollback();
-        if (err) {
-            console.log(err.message);
-            return res.json({
-                status: 400,
-                success: false,
-                message: 'Internal server error'
-            });
-        } else {
-        //
-          var request = new sql.Request(transaction);
-
-          request
-            .input('email', email)
-            .query(query_email, function (err, recordset) {
-                if (err) {
-                    console.log(err);
-                    transaction.rollback();
-                    return res.json({
-                        status: 500,
-                        success: false,
-                        message: 'Database error'
-
-                    });
-                } else {
-                    if (recordset.recordset.length > 0) {
-                        transaction.rollback();
-                        return res.json({
-                            status: 400,
-                            success: false,
-                            message: email + ' is already taken'
-                        });
-                    } else {
-                        //////
-                        request
-                            .input('email', email)
-                            .input('schoolname', schoolname)
-                            .input('address', address)
-                            .input('contacts', contacts)
-                            .input('datejoined', datejoined)
-                            .input('enrolmentkey', enrolmentkey)
-                            .query(query, function (err, recordset) {
-                                if (err) {
-                                    console.log(err);
-                                    transaction.rollback();
-                                    return res.json({
-                                        status: 500,
-                                        success: false,
-                                        message: 'Database error'
-
-                                    });
-                                } else {
-                                    if (recordset.rowsAffected[0] > 0) {
-                                        //if school added succ
-                                        schoolid = recordset.recordset[0].identity;
-
-                                        ////////////////////
-                                        request
-                                            .input('email', email)
-                                            .input('password', password)
-                                            .input('otp', pin)
-                                            .input('roleid', roleid)
-                                            .input('otpe', otpexpiry)
-                                            .input('dj', datejoined)
-                                            .query(query_user, function (err, recordset) {
-                                                if (err) {
-                                                    console.log(err);
-                                                    transaction.rollback();
-                                                    return res.json({
-                                                        status: 500,
-                                                        success: false,
-                                                        message: 'Database error'
-
-                                                    });
-                                                } else {
-                                                    if (recordset.rowsAffected[0] > 0) {
-                                                        //if user added succ
-                                                        userid = recordset.recordset[0].identity;
-                                                        ////////////////////////////////////////////////////
-                                                        request
-                                                            .input('firstname', firstname)
-                                                            .input('lastname', lastname)
-                                                            .input('userid', userid)
-                                                            .input('schoolid', schoolid)
-                                                            .input('dj', datejoined)
-                                                            .query(query_schooladmin, function (err, recordset) {
-                                                                if (err) {
-                                                                    console.log(err);
-                                                                    transaction.rollback();
-                                                                    return res.json({
-                                                                        status: 500,
-                                                                        success: false,
-                                                                        message: 'Database error'
-
-                                                                    });
-                                                                } else {
-                                                                    if (recordset.rowsAffected[0] > 0) {
-                                                                        return res.json({
-                                                                            status: 2001,
-                                                                            success: false,
-                                                                            message: 'school Added'
-                                                                        });
-
-                                                                    } else {
-
-                                                                        transaction.rollback();
-                                                                        return res.json({
-                                                                            status: 400,
-                                                                            success: false,
-                                                                            message: 'Failed to add school , admin. Rolled back changes'
-                                                                        });
-                                                                    }
-                                                                }
-                                                            });
+                return res.json({
+                    status: 200,
+                    success: true,
+                    data: JSON.parse(JSON.stringify({ teacher }))
+                });
+            }
+        });
 
 
-
-
-                                                        /////////////////////////////////////////////////////
-
-                                                    } else {
-
-                                                        transaction.rollback();
-                                                        return res.json({
-                                                            status: 400,
-                                                            success: false,
-                                                            message: 'Failed to add school , user. Rolled back changes'
-                                                        });
-                                                    }
-                                                }
-                                            });
-
-                                        //////////////////////
-
-                                    } else {
-                                  
-                                        transaction.rollback();
-                                        return res.json({
-                                            status: 400,
-                                            success: false,
-                                            message: 'Failed to add school. Rolled back changes'
-                                        });
-                                    }
-                                }
-                            });
-                        //////
-                    }
-                }
-            });
-        }
-    });
 }
 
-let del_school = (req, res) => {
+let del_teacher = (req, res) => {
     var id = req.params.id;
-    var query = "DELETE from [events] where EventId=@id";
+    var query = `delete from [teachers] where teacherId= ${id}`;
 
     request
-        .input("id", id)
         .query(query, function (err, recordset) {
 
             if (err) {
@@ -313,33 +154,25 @@ let del_school = (req, res) => {
 
 
 
-let update_school = (req, res) => {
+let update_teacher = (req, res) => {
+   let teacherid = req.body.teacherId;
+   let schoolid = req.body.schoolid;
+   let firstname = req.body.firstname;
+   let lastname = req.body.lastname;
+   let userid = req.body.userid;
+   let datejoined = req.body.datejoined;
    
-    var schoolid = req.body.id;
-    var schoolname = req.body.schoolname;
-    var contacts = req.body.contacts;
-    var address = req.body.address;
-    //var email = req.body.email;
-    //var enrolmentkey = generator.generate({
-    //    length: 5,
-    //    numbers: true
-    //});
-
-
-    let query = "UPDATE [schools] \
-    SET schoolname=@name \
-      SET contacts=@contacts \
-      SET address=@address \
-    WHERE schoolId = @id";
+    let query = `UPDATE [teachers] \
+    SET schoolid=${schoolid} \
+    SET firstname=${firstname} \
+    SET lastname=${lastname} \
+    SET userid=${userid} \
+    SET datejoined=${datejoined} \
+    WHERE teacherId = ${teacherid}`;
 
     var request = new sql.Request();
 
     request
-        .input("id", schoolid)
-        .input("name", schoolname)
-        .input("contacts", contacts)
-        .input("address", address)
-        .input("name", schoolname)
         .query(query, function (err, recordset) {
 
             if (err) {
@@ -371,5 +204,8 @@ let update_school = (req, res) => {
 
 module.exports = {
 	teacher,
-	teachers
+	teachers,
+	add_teacher,
+	del_teacher,
+	update_teacher
 };
