bdef97fbd922ef02d2c9e3da63496c375aa22129
keptac <55141570+keptac@users.noreply.github.com>
2020-07-29 15:21:10 +0200

    get resources

--- client/src/components/student-components/SubjectDescrip.js
+++ client/src/components/student-components/SubjectDescrip.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {StudentService} from '../../services/student';
+import { StudentService } from '../../services/student';
 
 // Search for downloadable resources from endpoint and display them here. Select resources from resource table where id = data
 export default class SubjectDescrip extends Component {
@@ -12,37 +12,48 @@ export default class SubjectDescrip extends Component {
     };
   }
 
-   data='';
-   
+  data = '';
+
   componentDidMount() {
     this.data = this.props.content;
     this.getDashData();
   }
 
-  getDashData(){
-     StudentService.get_course_downloadables(this.data)
-     .then((response) => {
-       console.log(response)
-       this.setState({ resources: response })
-     });
-    }
+  getDashData() {
+    StudentService.get_course_downloadables(this.data)
+      .then((response) => {
+        this.setState({ resources: response })
+      });
+  }
 
-    render() {
-        return this.state.resources.map((resource, i) => (
-            <div key={i}  className="col s12 m8 l4">
-              <div className="card min-height-100 white-text designed-dots" style={{borderRadius:"5px"}}>
-                <div className="padding-4">
-                <div className="col s12 m12">
-                    <p className="no-margin" style={{color:"teal",}}><b>{resource.materialname}</b></p>
-                    <p className = "no-margin" style={{fontSize:"12px", color:"grey"}}>{resource.dateadded}</p>
-                  </div>
-                  <div className="right-align" style={{marginTop:"60px",color:"black"}}>
-                    <p className="no-margin"><a href={resource.file} target="bank">DOWNLOAD</a></p>
-                  </div> 
-                </div>
-              </div>
-            </div>
-        )
-            )
+  download(resource){
+    var data = {
+      file:resource.file
     }
+    StudentService.download(data)
+      .then((response) => {
+        console.log(response);
+        // alert("Ddownload FInished");
+        // this.setState({ resources: response })
+      });
+  }
+
+  render() {
+    return this.state.resources.map((resource, i) => (
+      <div key={i} className="col s12 m8 l4">
+        <div className="card min-height-100 white-text designed-dots" style={{ borderRadius: "5px" }}>
+          <div className="padding-4">
+            <div className="col s12 m12">
+              <p className="no-margin" style={{ color: "teal", }}><b>{resource.materialname}</b></p>
+              <p className="no-margin" style={{ fontSize: "12px", color: "grey" }}>{resource.dateadded}</p>
+            </div>
+            <div className="right-align" style={{ marginTop: "60px", color: "black" }}>
+              <p className="no-margin"><a href={this.download(resource)} target="bank">DOWNLOAD</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+    )
+  }
 }
