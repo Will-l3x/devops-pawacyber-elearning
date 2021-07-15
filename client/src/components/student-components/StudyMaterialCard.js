063dd04b24d4286622eb79982ac0007ae37607f0
wilvanos zhira <lex540@live.com>
2021-07-15 12:43:20 +0200

    ie

--- client/src/components/student-components/StudyMaterialCard.js
+++ client/src/components/student-components/StudyMaterialCard.js
@@ -190,6 +190,7 @@ export default class StudyMaterialCard extends Component {
                         onChange={this.handleGradeDropdownChange}
                         required
                       >
+                         <option value="0">0</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
